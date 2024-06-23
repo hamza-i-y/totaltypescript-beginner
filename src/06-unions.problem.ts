be5a14b2cb0e5beeec34e2de39e62c70adfd7e89
hamza-i-y <hamzaiy@outlook.com>
2024-06-23 04:30:40 +0100

    Completed exercise 6

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -1,3 +1,5 @@
+type TRole = "admin" | "user" | "super-admin";
+
 interface User {
   id: number;
   firstName: string;
@@ -8,7 +10,7 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  role: TRole;
 }
 
 export const defaultUser: User = {
